9d69537a6b00a4cf2644790f29d0aa6c88191a6e
Geoffray Pellerin <85186987+Aspeckkt@users.noreply.github.com>
2022-10-26 08:52:24 -0400

    Add files via upload

--- backend/models/sauces.js
+++ backend/models/sauces.js
@@ -1,18 +1,17 @@
 const mongoose = require('mongoose');
-
 const sauceSchema = mongoose.Schema({
-    
+
     name: { type: String, required: true },
     manufacturer: { type: String, required: true },
     description: { type: String, required: true },
     mainPepper: { type: String, required: true },
     imageUrl: { type: String },
     heat: { type: Number },
-    likes: { type: Number, default: 0},
+    likes: { type: Number, default: 0 },
     dislikes: { type: Number, default: 0 },
     userId: { type: String },
     usersLiked: [String],
-    usersDisliked: [String] ,
+    usersDisliked: [String],
 
 });
 
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
+const mongoose = require('mongoose'); // Permet d'utiliser la base de donnée MongoDB
+const uniqueValidator = require('mongoose-unique-validator'); // Plugin qui oblige un utilisateur à n'avoir qu'un seul pseudo et adresse email sinon renvois une erreur
 
 const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
